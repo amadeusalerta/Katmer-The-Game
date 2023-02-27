cd64c0eb614718714d294dc6a04b407d8438e0ec
Amadeus Alerta <efe3365@gmail.com>
2023-02-27 15:41:55 +0300

    Update playerMovement.cs

--- Player Controllers/playerMovement.cs
+++ Player Controllers/playerMovement.cs
@@ -7,6 +7,15 @@ public class playerMovement : MonoBehaviour
     
     public CharacterController controller;
     public float speed = 10f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    public Vector3 velocity;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public bool isGrounded;
+
+
     void Start()
     {
         
@@ -14,9 +23,24 @@ public class playerMovement : MonoBehaviour
 
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x_axis = Input.GetAxis("Horizontal");
         float z_axis = Input.GetAxis("Vertical");
         Vector3 move=transform.right*x_axis+transform.forward*z_axis;
         controller.Move(move*speed*Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight*-2f*gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity*Time.deltaTime);
     }
 }
